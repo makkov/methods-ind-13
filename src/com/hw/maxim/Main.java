4a1a7f1d310c24dc2c03f3403cc03a6793661249
maks <maxim_86nva@mail.ru>
2023-07-01 13:42:16 +0300

    дз по методам

--- src/com/hw/maxim/Main.java
+++ src/com/hw/maxim/Main.java
@@ -1,8 +1,70 @@
 package com.hw.maxim;
 
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        //1
+        int leapYear = 2020;
+        int notLeapYear = 2021;
+        checkIsYearLeap(leapYear);
+        checkIsYearLeap(notLeapYear);
+
+        //2
+        int year = LocalDate.now().getYear();
+        int os = 0;
+        suggestVersion(os, year);
+
+        //3
+        printDeliveryInfo(1);
+        printDeliveryInfo(25);
+        printDeliveryInfo(130);
+    }
+
+    public static void checkIsYearLeap(int year) {
+        if (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0)){
+            System.out.println(year + " год не является високосным\n");
+        } else {
+            System.out.println(year + " год является високосным\n");
+        }
+    }
+
+    public static void suggestVersion(int os, int year) {
+        int borderYear = 2015;
+        if (os == 0 && year < borderYear) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (os == 0 && year >= borderYear) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (os == 1 && year < borderYear) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (os == 1 && year >= borderYear) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Не удалось определить версию");
+        }
+    }
+
+    public static void printDeliveryInfo(int deliveryDistance) {
+        int deliveryDays = getDeliveryDays(deliveryDistance);
+        if (deliveryDays == -1) {
+            System.out.println("Доставки нет");
+        } else {
+            System.out.printf("Потребуется %s дней", deliveryDays);
+        }
+        System.out.println();
+    }
+
+    public static int getDeliveryDays(int deliveryDistance) {
+        if (deliveryDistance < 20) {
+            return 1;
+        } else if (deliveryDistance <= 60) {
+            return 2;
+        } else if (deliveryDistance < 100) {
+            return 3;
+        } else {
+            return -1;
+        }
     }
 }
